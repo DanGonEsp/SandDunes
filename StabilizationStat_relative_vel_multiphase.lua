7e805beee627c92e3a371099afe423b1b7d76914
DanGon <daniel.gonzalezesparza@kaust.edu.sa>
2025-12-06 17:11:44 +0300

    Linesearch parameters

--- StabilizationStat_relative_vel_multiphase.lua
+++ StabilizationStat_relative_vel_multiphase.lua
@@ -44,7 +44,7 @@ AbsDefect = util.GetParamNumber("-AbsDefect", 1e-05)
 RedDefect = util.GetParamNumber("-RedDefect", 1e-05)
 damping_mg = util.GetParamNumber("-damping_mg", 1.0)
 value_beta = util.GetParamNumber("-value_beta", -0.4)
-lambdamaxSteps = util.GetParamNumber("-lambdamaxSteps", 8)
+lambdamaxSteps = util.GetParamNumber("-lambdamaxSteps", 5)
 lambdaStart  = util.GetParamNumber("-lambdaStart", 1.4)
 
 
